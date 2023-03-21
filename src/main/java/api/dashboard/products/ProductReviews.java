325ace5216091c933a7f9f41311c3a8a09c67cd0
Tien Van <tien.van@mediastep.com>
2023-03-21 09:22:49 +0700

    Added more functions to test product reviews

--- src/main/java/api/dashboard/products/ProductReviews.java
+++ src/main/java/api/dashboard/products/ProductReviews.java
@@ -1,13 +1,17 @@
 package api.dashboard.products;
 
-import io.restassured.response.Response;
-import utilities.api.API;
-
 import static api.dashboard.login.Login.accessToken;
 import static api.dashboard.login.Login.apiStoreID;
 
+import java.util.List;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import utilities.api.API;
+
 public class ProductReviews {
     String PRODUCT_REVIEW_CONFIG = "/itemservice/api/store-item-reviews/store/%s";
+    String PRODUCT_REVIEW_LIST = "/itemservice/api/item-reviews/store/storeID?langKey=vi&page=0&size=1000&sort=reviewDate,DESC&itemName.contains=";
     API api = new API();
     public static boolean isEnableReview;
 
@@ -17,4 +21,15 @@ public class ProductReviews {
 
         isEnableReview = productReviewsConfig.jsonPath().getBoolean("isEnableReview");
     }
+    
+    public JsonPath getAllReviewJsonPath() {
+    	Response productReviews = api.get(PRODUCT_REVIEW_LIST.replace("storeID", String.valueOf(apiStoreID)), accessToken);
+    	productReviews.then().statusCode(200);
+    	return productReviews.jsonPath();
+    }
+    
+    public List<String> getProductNameList() {
+    	return getAllReviewJsonPath().getList("itemName");
+    }
+
 }
