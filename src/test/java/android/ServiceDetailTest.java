4f7b22a8c468ee2c9dc57527153403175cbc94a7
VTTV17 <vttv17@gmail.com>
2023-04-04 10:17:43 +0700

    update service detail test

--- src/test/java/android/ServiceDetailTest.java
+++ src/test/java/android/ServiceDetailTest.java
@@ -1,15 +1,39 @@
 package android;
 
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.buyerapp.BuyerGeneral;
 import pages.buyerapp.NavigationBar;
 import pages.buyerapp.search.BuyerSearchDetailPage;
 import pages.buyerapp.search.BuyerSearchPage;
 
-public class ServiceDetailTest extends AppiumTest{
+import java.net.URL;
+
+public class ServiceDetailTest {
     NavigationBar navigationBar;
     BuyerSearchPage searchPage;
     BuyerSearchDetailPage searchDetailPage;
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "Android");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", "11.0");
+        caps.setCapability("udid", "R5CR92R4K7V");
+//        caps.setCapability("appPackage", "com.mediastep.GoSellForSeller.STG");
+//        caps.setCapability("appActivity", "com.mediastep.gosellseller.modules.credentials.login.LoginActivity");
+        caps.setCapability("appPackage", "com.mediastep.shop0003");
+        caps.setCapability("appActivity", "com.mediastep.gosell.ui.modules.splash.SplashScreenActivity");
+
+        caps.setCapability("noReset", "false");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+    }
     @Test
     public void demo(){
         BuyerGeneral general = new BuyerGeneral(driver);
