e86a0a23995dc6ca32c707459628725edd071b70
VTTV17 <vttv17@gmail.com>
2023-06-22 14:24:11 +0700

    update element and new testcase

--- src/main/java/pages/buyerapp/account/BuyerMyProfile.java
+++ src/main/java/pages/buyerapp/account/BuyerMyProfile.java
@@ -22,18 +22,19 @@ public class BuyerMyProfile extends BuyerMyProfileElement{
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         common = new UICommonMobile(driver);
     }
-    public BuyerMyProfile verifyText() throws Exception {
+    public BuyerMyProfile verifyTextMyProfile() throws Exception {
+
         Assert.assertEquals(common.getText(MY_PROFILE_HEADER_TITLE), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.pageTitle"));
         Assert.assertEquals(common.getText(MY_PROFILE_HEADER_SAVE_BTN), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.saveBtn"));
         Assert.assertEquals(common.getText(YOUR_NAME_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.yourNameLbl"));
         Assert.assertEquals(common.getText(EMAIL_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.emailLbl"));
         Assert.assertEquals(common.getText(IDENTITY_CARD_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.identityCardLbl"));
         Assert.assertEquals(common.getText(OTHER_EMAILS_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherEmailsLbl"));
-        Assert.assertEquals(common.getText(PHONE_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.phoneLbl"));
-        Assert.assertEquals(common.getText(OTHER_PHONE_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherPhoneNumbersLbl"));
-        Assert.assertEquals(common.getText(COMPANY_NAME_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.companyNameLbl"));
+        verifyTextOtherEmailPage();
         Assert.assertEquals(common.getText(PHONE_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.phoneLbl"));
         common.swipeByCoordinatesInPercent(0.75,0.75,0.25,0.25);
+        verifyTextOtherPhonePage();
+        Assert.assertEquals(common.getText(COMPANY_NAME_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.companyNameLbl"));
         Assert.assertEquals(common.getText(TAX_CODE_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.taxCodeLbl"));
         Assert.assertEquals(common.getText(PROFILE_ADDRESS_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.addressLbl"));
         Assert.assertEquals(common.getText(GENDER_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.genderLbl"));
@@ -42,6 +43,48 @@ public class BuyerMyProfile extends BuyerMyProfileElement{
         Assert.assertEquals(common.getText(BIRTHDAY_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.birthdayLbl"));
         Assert.assertEquals(common.getText(CHANGE_PASSWORD_LBl), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.changePassword"));
         Assert.assertEquals(common.getText(DELETE_ACCOUNT_LBL), PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.deleteAccount"));
+        verifyTextAddressPage();
         return this;
+    }
+    public void verifyTextOtherEmailPage() throws Exception {
+        common.clickElement(YOU_HAVE_OTHER_MAIL_LBL);
+        Assert.assertEquals(common.getText(OTHER_EMAIL_TITLE),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherEmail.pageTitle"));
+        common.clickElement(OTHER_EMAIL_ADD_ICON);
+//        Assert.assertEquals(common.getText(OTHER_EMAIL_POPUP_TITLE),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherEmail.addEmailPopupTitle"));
+        Assert.assertEquals(common.getText(OTHER_EMAIL_INPUT_NAME),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherEmail.enterNameHint"));
+        Assert.assertEquals(common.getText(OTHER_EMAIL_INPUT_EMAIL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherEmail.enterEmailHint"));
+        Assert.assertEquals(common.getText(OTHER_EMAIL_ADD_BTN),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherEmail.addBtn"));
+        common.clickElement(OTHER_EMAIL_POPUP_CLOSE_ICON);
+        common.clickElement(OTHER_EMAIL_BACK_ICON);
+    }
+    public void verifyTextOtherPhonePage() throws Exception {
+        common.clickElement(YOUR_HAVE_OTHER_PHONE_LBL);
+        Assert.assertEquals(common.getText(OTHER_PHONE_TITLE),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherPhone.pageTitle"));
+        common.clickElement(OTHER_PHONE_ADD_ICON);
+        Assert.assertEquals(common.getText(OTHER_PHONE_POPUP_TITLE),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherPhone.addPhonePopupTitle"));
+        Assert.assertEquals(common.getText(OTHER_PHONE_INPUT_NAME),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherPhone.enterNameHint"));
+        Assert.assertEquals(common.getText(OTHER_PHONE_INPUT_PHONE),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherPhone.enterPhoneHint"));
+        Assert.assertEquals(common.getText(OTHER_PHONE_ADD_BTN),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.otherPhone.addBtn"));
+        common.clickElement(OTHER_PHONE_POPUP_CLOSE_ICON);
+        common.clickElement(OTHER_PHONE_BACK_ICON);
+    }
+    public void verifyTextAddressPage() throws Exception {
+        common.clickElement(ADDRESS_TXT);
+        Assert.assertEquals(common.getText(ADDRESS_HEADER_TITLE),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.pageTitle"));
+        Assert.assertEquals(common.getText(ADDRESS_HEADER_SAVE_BTN),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.saveBtn"));
+        Assert.assertEquals(common.getText(COUNTRY_LBL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.countryLbl"));
+        Assert.assertEquals(common.getText(ADDRESS_LBL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.addressLbl"));
+        Assert.assertEquals(common.getText(CITY_PROVINCE_LBL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.cityProvinceLbl"));
+        Assert.assertEquals(common.getText(DISTRICT_LBL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.districtLbl"));
+        Assert.assertEquals(common.getText(WARD_LBL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.wardLbl"));
+        common.clickElement(COUNTRY_DROPDOWN);
+        Assert.assertEquals(common.getText(COUNTRY_HEADER_TITLE),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.country.pageTitle"));
+        common.clickElement(COUNTRY_LIST);
+        Assert.assertEquals(common.getText(ADDRESS_2_LBL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.address2Lbl"));
+        Assert.assertEquals(common.getText(STATE_REGION_PROVINCE_LBL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.stateRegionProviceLbl"));
+        Assert.assertEquals(common.getText(CITY_LBL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.cityLbl"));
+        Assert.assertEquals(common.getText(ZIP_CODE_LBL),PropertiesUtil.getPropertiesValueBySFLang("buyerApp.myProfile.address.zipCodeLbl"));
+
+
     }
 }
