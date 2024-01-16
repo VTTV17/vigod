a11427d908d666c71e827a16db7ee0bb32fd50e9
Tien Van <tien.van@mediastep.com>
2024-01-16 09:37:57 +0700

    Updated WebElements to By objects

--- src/main/java/pages/dashboard/saleschannels/lazada/AccountManagement.java
+++ src/main/java/pages/dashboard/saleschannels/lazada/AccountManagement.java
@@ -1,15 +1,9 @@
 package pages.dashboard.saleschannels.lazada;
 
-import java.time.Duration;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.asserts.SoftAssert;
 
 import pages.dashboard.confirmationdialog.ConfirmationDialog;
 import pages.dashboard.home.HomePage;
@@ -19,52 +13,39 @@ public class AccountManagement {
 
 	final static Logger logger = LogManager.getLogger(AccountManagement.class);
 	
-	
     WebDriver driver;
-    WebDriverWait wait;
     UICommonAction commonAction;
 
-    SoftAssert soft = new SoftAssert();    
-    
     public AccountManagement(WebDriver driver) {
         this.driver = driver;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         commonAction = new UICommonAction(driver);
-        PageFactory.initElements(driver, this);
     }
 
-    @FindBy(xpath = "//div[contains(@class,'ShopeeAccountManagement')]//i[contains(@style,'broken-link.svg')]")
-    WebElement DISCONNECT_BTN;	
-    
-    @FindBy(xpath = "//div[contains(@class,'ShopeeAccountManagement')]//i[contains(@style,'icon-delete')]")
-    WebElement DELETE_ACCOUNT_BTN;	
-    
-    @FindBy(css = ".boxCheckedDelete .uik-checkbox__label.green")
-    WebElement DELETE_SYNCED_PRODUCT_CHK;	
-    
-    @FindBy(css = ".modal-footer .gs-button__red")
-    WebElement DELETE_BTN;	
+    By loc_btnDisconnect = By.xpath("//div[contains(@class,'ShopeeAccountManagement')]//i[contains(@style,'broken-link.svg')]");
+    By loc_btnDeleteAccount = By.xpath("//div[contains(@class,'ShopeeAccountManagement')]//i[contains(@style,'icon-delete')]");
+    By loc_chkDeleteSyncedProduct = By.cssSelector(".boxCheckedDelete .uik-checkbox__label.green");
+    By loc_btnDelete = By.cssSelector(".modal-footer .gs-button__red");
     
     public AccountManagement clickDisconnect() {
-    	commonAction.clickElement(DISCONNECT_BTN);
+    	commonAction.click(loc_btnDisconnect);
     	logger.info("Clicked on 'Disconnect Shopee' button.");
     	return this;
     }      
     
     public AccountManagement clickDeleteConnectedAccount() {
-    	commonAction.clickElement(DELETE_ACCOUNT_BTN);
+    	commonAction.click(loc_btnDeleteAccount);
     	logger.info("Clicked on 'Delete Connected Account' button.");
     	return this;
     }   
     
     public AccountManagement clickDeleteSyncedProductCheckbox() {
-    	commonAction.clickElement(DELETE_SYNCED_PRODUCT_CHK);
+    	commonAction.click(loc_chkDeleteSyncedProduct);
     	logger.info("Clicked on 'Delete Synced Products' check box.");
     	return this;
     }     
     
     public AccountManagement clickDeleteBtnInConfirmationDialog() {
-    	commonAction.clickElement(DELETE_BTN);
+    	commonAction.click(loc_btnDelete);
     	logger.info("Clicked on 'Delete' button in Confirmation Dialog.");
     	return this;
     }      
--- src/main/java/pages/dashboard/saleschannels/lazada/LinkProducts.java
+++ src/main/java/pages/dashboard/saleschannels/lazada/LinkProducts.java
@@ -1,15 +1,9 @@
 package pages.dashboard.saleschannels.lazada;
 
-import java.time.Duration;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.asserts.SoftAssert;
 
 import pages.dashboard.home.HomePage;
 import utilities.UICommonAction;
@@ -18,25 +12,18 @@ public class LinkProducts {
 
 	final static Logger logger = LogManager.getLogger(LinkProducts.class);
 	
-	
     WebDriver driver;
-    WebDriverWait wait;
     UICommonAction commonAction;
 
-    SoftAssert soft = new SoftAssert();    
-    
     public LinkProducts(WebDriver driver) {
         this.driver = driver;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         commonAction = new UICommonAction(driver);
-        PageFactory.initElements(driver, this);
     }
 
-	@FindBy(xpath = "//div[@class='ml-auto']/preceding-sibling::*//input")
-	WebElement SEARCH_BOX;    
+    By loc_txtSearch = By.xpath("//div[@class='ml-auto']/preceding-sibling::*//input");
     
 	public LinkProducts inputSearchTerm(String searchTerm) {
-		commonAction.inputText(SEARCH_BOX, searchTerm);
+		commonAction.sendKeys(loc_txtSearch, searchTerm);
 		logger.info("Input '" + searchTerm + "' into Search box.");
 		new HomePage(driver).waitTillSpinnerDisappear();
 		return this;
